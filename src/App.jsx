a3bbd1754290deba55cbe8ba407141cd3e419b71
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-03-14 16:25:50 +0000

    initial building blocks of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
 
 function App() {
+  const todos = [
+    { id: 1, title: "learn react in 12 weeks" },
+    { id: 2, title: "learn node in 13 weeks after react" },
+    { id: 3, title: "learn typescript whiles learning react" },
+  ];
   return (
     <div>
       <h1>My Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
