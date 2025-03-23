c3b28ece23152b655313328d91582b5e4c462b48
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-03-23 19:49:59 +0000

    revise: better semantics and functionally to form tag

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,10 @@
 function TodoForm() {
   return (
-    <>
+    <form>
       <label htmlFor="todoTitle">Todo</label>
-      <input id="todoTitle" />
+      <input id="todoTitle" placeholder="Please add your todo's"/>
       <button>Add Todo</button>
-    </>
+    </form>
   );
 }
 export default TodoForm;
