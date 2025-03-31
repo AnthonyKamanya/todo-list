72b9149f64e678aea4d8b1961728f13e89352a6d
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-03-31 17:56:41 +0100

    chore: Add onClick or onSubmit functionality for learning purposes

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,17 @@
 function TodoForm() {
   return (
-    <form>
+    <form /*onSubmit={(event) => onsubmit(event)} this is a button submit action*/
+    >
       <label htmlFor="todoTitle">Todo</label>
-      <input id="todoTitle" placeholder="Please add your todo's"/>
-      <button>Add Todo</button>
+      <input id="todoTitle" placeholder="Please add your todo's" />
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          console.log("How to make buttons click");
+        }}
+      >
+        Add Todo
+      </button>
     </form>
   );
 }
