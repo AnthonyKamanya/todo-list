6f1a3e21cafcf282c638c813b5ddd49aba05f71f
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-02 23:17:10 +0100

    Chore: remove unused functionalities

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,17 +1,9 @@
 function TodoForm() {
   return (
-    <form /*onSubmit={(event) => onsubmit(event)} this is a button submit action*/
-    >
+    <form>
       <label htmlFor="todoTitle">Todo</label>
-      <input id="todoTitle" placeholder="Please add your todo's" />
-      <button
-        onClick={(event) => {
-          event.preventDefault();
-          console.log("How to make buttons click");
-        }}
-      >
-        Add Todo
-      </button>
+      <input id="todoTitle" />
+      <button>Add Todo</button>
     </form>
   );
 }
