c551752e0ff5955b695ad544165c6e3715df5029
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-10 23:00:30 +0100

    refactor: remove the mock data used in the Todolist for iterations

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,12 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList() {
-  const todos = [
-    { id: 1, title: "learn react in 12 weeks", startDate: "11-03-25" },
-    {
-      id: 2,
-      title: "learn node in 13 weeks after react",
-      startDate: "02-06-25",
-    },
-    {
-      id: 3,
-      title: "learn typescript whiles learning react",
-      startDate: "18-03-25",
-    },
-  ];
-
+function TodoList({ todoList }) {
   return (
     <ul>
-      {todos.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo}  startDate={todo.startDate} />
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id}  todo={todo}>
+          {todo}
+        </TodoListItem>
       ))}
     </ul>
   );
