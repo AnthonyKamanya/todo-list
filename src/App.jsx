1be16b2edb324509d2918a60b8aeb55af1221626
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-20 20:10:27 +0100

    wip: updatedTodo now set to setTodoList and helper function passed down as a prop to todoList

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,21 @@ function App() {
   const [todoList, setTodoList] = useState([]);
   function handleAddTodo(newTodo) {
     const title = event.target.title.value;
-    newTodo = { title:title, id: Date.now(), isCompleted: false };
+    newTodo = { title: title, id: Date.now(), isCompleted: false };
     setTodoList([...todoList, newTodo]);
   }
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: true } : todo
+    );
+    setTodoList(updatedTodos)
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={handleAddTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
     </div>
   );
 }
