cbfc7da85626c640f2d515807eb5950a6b8de5fd
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-21 00:15:45 +0100

    wip. invoked a parameter from a prop passed down from a grandparent component

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,6 +1,14 @@
 function TodoListItem({ todo, onCompleteTodo }) {
+  function handleChange(){
+    onCompleteTodo(todo.id)
+  }
   return (
     <li id={todo.id}>
+      <input
+        type="checkbox"
+        checked={todo.isCompleted}
+        onChange={handleChange}
+      />
       <form>{todo.title}</form>
     </li>
   );
