2a93af2c9cd72840390b3c9a412b6a04844bec69
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-21 00:40:17 +0100

    wip: manage state by passing event's target value to setWorkingTodoTitle connect form input to component state

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,25 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-function TodoForm({ onAddTodo, }) {
-  const todoTitleInput = useRef("")
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef("");
+  const [workingTodoTitle, setWorkingTodoTitle] = useState("");
 
-  function handleAddTodo(event) {
-    event.preventDefault();
-    const title = event.target.title.value;
-    const id = Date.now()
-    onAddTodo({title, id}); //This [id: Date.now] creates a uniq Id
-    event.target.title.value = "";
-    todoTitleInput.current.focus()
+  function handleAddTodo(title) {
+    title.preventDefault();
+    const id = Date.now();
+    onAddTodo({ workingTodoTitle, id });
+    setWorkingTodoTitle("");
+    todoTitleInput.current.focus();
   }
 
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
       <input
+        value={workingTodoTitle}
         id="todoTitle"
         name="title"
         placeholder="Please enter your text "
         ref={todoTitleInput}
+        onChange={(event) => setWorkingTodoTitle(event.target.title.value)}
       />
       <button>Add Todo</button>
     </form>
