6dacd594112ad9647153327ac97c6e17acac0fe4
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-27 02:06:48 +0100

    feat: 1 Allow Users to Edit Existing Todos 1. a Toggle Between Todo Display and Edit

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -3,14 +3,28 @@ import TextInputWithLabel from "../../shared/TextInputWithLabel";
 
 function TodoListItem({ todo, onCompleteTodo }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
   function handleChange() {
     onCompleteTodo(todo.id);
   }
+
+  function handleCancel() {
+    workingTitle = todo.title;
+    setIsEditing(false);
+  }
+
+  function handleEdit(){}
   return (
     <li id={todo.id}>
       <form>
         {isEditing ? (
-          <TextInputWithLabel value={todo.title} />
+          <>
+            <TextInputWithLabel value={todo.title} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
         ) : (
           <>
             <label>
