13ea04a4a45be5b81d3e645cbe08fbbe75b3ff9c
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-05-03 07:07:55 +0100

    chore: loading todos from airtable Establishing Transition state Fetching in App Displaying Error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import "./App.css";
 import TodoList from "./features/TodoList/TodoList.jsx";
 import TodoForm from "./features/TodoForm.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading({ isLoading: true });
+      const options = { method: "GET", headers: { Authorization: token } };
+      try {
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          throw new Error(resp.message);
+        }
+        const { records } = await resp.json();
+        const fetchedTodos = records.map((record) => {
+          const todo = {
+            id: record.id,
+            ...record.fields,
+          };
+          if (!todo.isCompleted) {
+            todo.isCompleted = false;
+          }
+          return todo;
+        });
+        setTodoList([...fetchedTodos]);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
   function handleAddTodo(title) {
     const newTodo = { title: title, id: Date.now(), isCompleted: false };
     setTodoList([...todoList, newTodo]);
@@ -28,9 +62,17 @@ function App() {
       <TodoForm onAddTodo={handleAddTodo} />
       <TodoList
         todoList={todoList}
+        isLoading={isLoading}
         onCompleteTodo={completeTodo}
         onUpdateTodo={handleUpdateTodo}
       />
+      {errorMessage && (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button onClick={() => setErrorMessage("")}>Dismiss</button>
+        </div>
+      )}
     </div>
   );
 }
